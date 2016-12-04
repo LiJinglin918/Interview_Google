bf0a48f3bdb73e346295e1c18ae3ae0d91c804f1
LiJinglin918 <lijinglin918@gmail.com>
2016-12-04 08:39:29 +0700

    Update BSTsubtreeRange.java

--- BSTsubtreeRange.java
+++ BSTsubtreeRange.java
@@ -1,5 +1,31 @@
 import java.util.*;
 
+// 两种方法，第一种更简洁。
+
+public class getSubTreeSize {
+	public int Result;
+	public int solution(TreeNode root, int left, int right) {
+		Result = 0;						// 全局变量可以变成-1.                          
+		helper(root, left, right);
+		return Result;
+	}
+	public int helper(TreeNode root, int left, int right) {
+		if (root == null)
+			return 0;
+		int left_num = helper(root.left, left, right);
+		int right_num = helper(root.right, left, right);
+		int res = -1;
+		if (left_num >= 0 && right_num >= 0 && left <= root.val && right >= root.val) {
+			res = left_num + right_num + 1;
+			Result = Math.max(Result, res);
+		}
+		return res;
+	}
+}
+
+
+/*====================================================================================================================*/
+
 
 public class BSTsubtreeRange {
 	static int count = 0;
